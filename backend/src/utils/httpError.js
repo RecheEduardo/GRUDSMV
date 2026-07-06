e1f5678f853f382277432c632a6c4cd709c2b214
recheeduardo <eduardorechemartins2006@gmail.com>
2026-07-06 17:50:44 -0300

    refact: update httpError function to use ES module export syntax

--- backend/src/utils/httpError.js
+++ backend/src/utils/httpError.js
@@ -1,8 +1,6 @@
 // Cria um Error com status HTTP associado, tratado pelo middleware errorHandler.
-function httpError(status, message) {
+export default function httpError(status, message) {
   const err = new Error(message);
   err.status = status;
   return err;
 }
-
-module.exports = httpError;
